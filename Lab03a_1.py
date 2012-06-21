9eea5a5616a600d7f3be30efdcf52caf4eb1019f
AITI Student <aiti-student@aiti-090-PC.(none)>
2012-06-21 05:23:46 -0400

    just made more  changes

--- Lab03a_1.py
+++ Lab03a_1.py
@@ -1,19 +1,51 @@
-import math
-number_tobe=input('input numbers for a reverse encryption')
+'''
+Created on Jun 21, 2012
 
-width=math.ceil(math.log(number_tobe,10))
-width=int(width)
-check=width-1
-encrypt=int(number_tobe)
-ans=0
+@author: william
+'''
+#EXTRA CREDIT
+import math  #IMPORTING MATH MODULE
 
-for i in range(width):
-    value=encrypt/10
-    remainder=encrypt%10
-    ans=ans +remainder *10**(check-1)
-    encrypt=value
-print ans
+number=raw_input("Pleas input numbers")
+number_int=int(number)
+length_num=int((math.log(number_int,10))) + 1
+add_length=length_num
+reverse=0
+reverse2=0
 
-    
-    
+for i in range(length_num):
+    num_step1=number_int/10
+    num_remainder=number_int%10
+    add_length=add_length-1
+    reverse+=num_remainder *(10**(add_length))
+    number_int=num_step1
+print reverse  
+
+
+
+#QUESTION ON EXERCISE TWO
+add_length=length_num #TRYING TO GET LENGTH
+number_int=int(number) #CONVERTING INT TO NUMBERS
+for i in range(length_num):
+    num_step1=number_int/10
+    num_remainder=number_int%10
+    num_remainder=(num_remainder+7)%10
+    add_length=add_length-1
+    reverse2+=num_remainder *(10**(add_length))
+    number_int=num_step1
+print reverse2 #PRINTING ANSWER EG 12345 BECOMES 89012 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
